562af4c86df51cae366072cb87ac166c0d4d389f
8kTuvaN <148682220+8kTuvaN@users.noreply.github.com>
2024-05-20 15:42:39 +0800

    Update video_summary.py

--- video_summary.py
+++ video_summary.py
@@ -1,27 +1,28 @@
 import os
 
 import openai
-import ffmpeg
+from openai import OpenAI
+# import ffmpeg
 import subprocess
 
 os.environ['OPENAI_API_KEY'] = ''
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 
-def mp4_to_mp3(mp4_path, output_path):
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    mp3_path = os.path.join(output_path, "audio.mp3")
-
-    command = f'ffmpeg -i "{mp4_path}" -vn -ar 44100 -ac 2 -b:a 192k "{mp3_path}"'
-    subprocess.call(command)
-
-    try:
-        subprocess.run(command, check=True)
-        print(f"Audio extracted and saved to {mp3_path}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred: {e}")
+# def mp4_to_mp3(mp4_path, output_path):
+#
+#     if not os.path.exists(output_path):
+#         os.makedirs(output_path)
+#     mp3_path = os.path.join(output_path, "audio.mp3")
+#
+#     command = f'ffmpeg -i "{mp4_path}" -vn -ar 44100 -ac 2 -b:a 192k "{mp3_path}"'
+#     subprocess.call(command)
+#
+#     try:
+#         subprocess.run(command, check=True)
+#         print(f"Audio extracted and saved to {mp3_path}")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error occurred: {e}")
 
 
 def summary(path):
@@ -49,8 +50,19 @@ def subtitle_summary(path):
     )
     return transcription.text
 
+def summarize(s):
+    client = OpenAI()
+    prompt = "summarize the following subtitles from a single shot: " + s
+    return client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    ).choices[0].message.content
+
 
 if __name__ == '__main__':
     # mp4_to_mp3('test_sources/test_video/test01.mp4', 'test_sources/mp3files')
-    S = subtitle_summary('test_sources/mp3files/audio.mp3')
-    print(S)
+    S = subtitle_summary('C:/Users/33615/Desktop/Test.mp4')
+    result = summarize(S)
+    print(result)
